194465f7100f26e5275d583deec6fac3e8a8c0b1
rafaelcunha02 <rafaelscpcunha@gmail.com>
2024-07-22 13:36:03 +0100

    reply styling done

--- src/components/ProductPage/CommentSection.js
+++ src/components/ProductPage/CommentSection.js
@@ -45,58 +45,82 @@ function CommentSection({ user }) {
     const toggleReplyForm = (event) => {
     // Handle reply form here
     }
+  
+    const publishReply = (event) => {
+    // Handle reply publishing here
+    }
 
-  return (
-    <section id="comments">
-      <h1 style={{ textAlign: 'center' }}>Questions & Answers</h1>
-      {user && (
-        <form onSubmit={publishComment} className="commentForm">
-          <textarea
-            name="content"
-            id="commentContent"
-            placeholder="Ask a Question..."
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <button id="commentSubmit" type="submit">
-            Submit
-          </button>
-        </form>
-      )}
+return (
+  <section id="comments">
+    <h1 style={{ textAlign: 'center' }}>Questions & Answers</h1>
+    {user && (
+      <form onSubmit={publishComment} className="commentForm">
+        <textarea
+          name="content"
+          id="commentContent"
+          placeholder="Ask a Question..."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button id="commentSubmit" type="submit">
+          Submit
+        </button>
+      </form>
+    )}
 
-      <ul id="commented">
-        {comments.map((comment) => {
-          const user = comment.User; // Fetch user data here
-          return (
-            <li key={comment.commentID}>
-              <img src="../assets/placeholder-1-1.webp" alt="User" />
-              <div className="comment">
-                <div className="commentUser">
-                  {user.firstName} {user.lastName}
-                </div>
-                <div className="commentContent">{comment.content}</div>
-
-                {user && (
-                  <div>
-                    <button className="responder" onClick={toggleReplyForm}>
-                      Reply
-                    </button>
-                  </div>
-                )}
-
-                <div>
-                  <button className="seeReplies" onClick={toggleReplies}>
-                    <span>&#x25BC;</span> Replies
-                  </button>
-                </div>
-                {/* Handle replies here */}
+    <ul id="commented">
+      {comments.map((comment) => {
+        const user = comment.User; // Fetch user data here
+        return (
+          <li key={comment.id}>
+            <img src="../assets/placeholder-1-1.webp" alt="User" />
+            <div className="comment">
+              <div className="commentUser">
+                {user.firstName} {user.lastName}
               </div>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+              <div className="commentContent">{comment.content}</div>
+
+              {user && (
+                <div>
+                  <button className="responder" onClick={toggleReplyForm}>
+                    Reply
+                  </button>
+                  <form id="replyForm" className="commentForm" onSubmit={publishReply}>
+                    <textarea
+                      name="replyContent"
+                      id="replyContent"
+                      placeholder="Reply to this Question..."
+                    />
+                    <button id="replySubmit" type="submit">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              )}
+
+              <div>
+                <button className="seeReplies" onClick={toggleReplies}>
+                  <span>&#x25BC;</span> Replies
+                </button>
+              </div>
+              <div className="replies">
+                <form id="replyForm" className="commentForm" style={{ display: 'none' }} onSubmit={publishReply}>
+                  <textarea className="areaReply" name="content" placeholder="Reply to this Question..." />
+                  <div className="alignButtonReply">
+                    <button id="replySubmit" type="submit">Submit</button>
+                  </div>
+                </form>
+                <ul id="replysUl" style={{ display: 'none' }}>
+                  {/* Handle replies here */}
+                </ul>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+);
 }
 
 export default CommentSection;
